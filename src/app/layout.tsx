31de0e8620a3e142fc4a725468dbd6db45747d0c
fjribptra <bagjaputrafajri@gmail.com>
2025-03-01 22:10:49 +0700

    feat: add meta google analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="BaZI1dEoU-chgF1oO_CktflBxhmfCnUEsop82e3DkL8" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
